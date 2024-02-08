f8d66327188b30609adef6eea0a5402b2ae129af
junkor-1011 <tomoyukizxy@gmail.com>
2024-02-09 00:58:26 +0900

    powertools logger

--- workspace/lambda/functions/hello/get.ts
+++ workspace/lambda/functions/hello/get.ts
@@ -1,10 +1,15 @@
 import 'source-map-support';
 
+import { Logger } from '@aws-lambda-powertools/logger';
 import type { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+
+const logger = new Logger({ serviceName: 'hello-world-get' });
 
 export const lambdaHandler = async (
   _event: APIGatewayProxyEvent,
 ): Promise<APIGatewayProxyResult> => {
+  logger.info('lambda init');
+
   const response = {
     statusCode: 200,
     body: JSON.stringify({
