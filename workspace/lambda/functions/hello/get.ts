029d7561e0b99433e1bf8423306621f75cc100e5
junkor-1011 <tomoyukizxy@gmail.com>
2024-02-12 14:15:26 +0900

    add tracer & metrics

--- workspace/lambda/functions/hello/get.ts
+++ workspace/lambda/functions/hello/get.ts
@@ -1,13 +1,25 @@
 import 'source-map-support';
 
 import { Logger } from '@aws-lambda-powertools/logger';
+import { MetricUnits, Metrics } from '@aws-lambda-powertools/metrics';
+import { Tracer } from '@aws-lambda-powertools/tracer';
 import type { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
-const logger = new Logger({ serviceName: 'hello-world-get' });
+const serviceName = 'hello-world-get'; // TMP
+
+const logger = new Logger({ serviceName });
+const tracer = new Tracer({ serviceName });
+const metrics = new Metrics({
+  namespace: 'hello-world',
+  serviceName,
+});
 
 export const lambdaHandler = async (
   _event: APIGatewayProxyEvent,
 ): Promise<APIGatewayProxyResult> => {
+  tracer.getSegment();
+
+  metrics.addMetric('lambdaInvoke', MetricUnits.Count, 1);
   logger.info('lambda init');
 
   const response = {
